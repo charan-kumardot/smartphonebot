5a35e1b7f5e26340c9a38cc464efe854f91c2e9f
charan-kumardot <59021375+charan-kumardot@users.noreply.github.com>
2021-01-19 18:31:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__, template_folder='templates')
 
-class Welcome:
-    def __init__(self,idd):
-        self.id=idd
-    def welcome():
-        return '''
-        ברוכים הבאים למגן סלולרי בהשגחה פרטית, איך אפשר לעזור? 
-        לעזרה ברכישת מכשיר לחץ 1
-לעזרה בנושאי שירות לקוחות לחץ 2
-תרצו לדבר עם מנהל מכירות לחץ 3
-        '''
-obj=[]
+
 
 pairs =[
 
@@ -37,10 +27,13 @@ pairs =[
 תקשורת טובה היא שם המשחק תרגישו חופשי לשתף אותנו 
 תשאלו תבררו אנחנו כאן לעזור לכם… במייל ובטלפון איך שנוח לכם.
         ''']],
-        ['(3)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
-
-        ['(|טלפון כשר|פלאפון כשר|יוטיוב כשר|סמארטפון כשר|טלפון לילדים|נוקיה 3310 כשר|טאבלט כשר|סלולר כשר|סים כשר|שיאומי כשר|טלפון כשר מאושר|טלפון כשר עם וואטסאפ|טלפון כשר מאושר|מושגח פלוס|פלאפון מושגח|סמארטפון מוגן|וויז כשר|טוקמן|סמארטפון כשר זול|סמארטפון כשר ועדת הרבנים|שיאומי תומך כשר|יוטיוב כשר להורדה|מכשיר כשר|סלולרי כשר|מושגח|טלפונים כשרים|פלאפונים כשרים|ACER|ALCATEL|ALLVIEW|AMAZONAMOI|APPLE|ARCHOS|ASUS|AT&T|BENEFON|BENQ|BENQ-SIEMENS|BIRD|BLACKBERRY|BLACKVIEW|BLUBOSCH|BQ|CASIO|CAT|CELKON|CHEA|COOLPAD|DELL|EMPORIA|ENERGIZER|ERICSSON|ETEN|FAIRPHONE|FUJITSU-SIEMENS|GARMIN-ASUS|GIGABYTE|GIONEE|GOOGLE|HAIER|HONOR|HPHTC|HUAWEI|I-MATE|I-MOBILE|ICEMOBILE|INFINIX|INQ|INTEX|JOLLA|KARBONN|KYOCERA|LAVA|LEECO|LENOVO|LG|MAXON|MAXWEST|MEIZU|MICROMAX|MICROSOFT|MITAC|MITSUBISHI|MODU|MOTOROLA|MWG|NEC|NEONODE|NIU|NOKIA|NVIDIA|ONEPLUS|OPPO|ORANGE|PALM|PANASONIC|PANTECH|PARLA|PHILIPS|PLUMPOSH|PRESTIGIO|QMOBILE|QTEK|RAZER|REALME|SAGEM|SAMSUNG|SENDO|SEWON|SHARP|SIEMENS|SONIM|SONY|SONY ERICSSON|SPICET-MOBILE|TCL|TECNO|TEL.ME.|TELIT|THURAYA|TOSHIBA|ULEFONE|UNNECTOVERT|UVERYKOOL|VIVO|VK MOBILE|VODAFONE|WIKO|WND|XCUTE|XIAOMI|XOLO|YEZZ|YOTA|YUZTE)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
-
+        ['(3|(.*)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
+        ['(ברוך הבא|בוקר טוב|היי|)',['''
+        ברוכים הבאים למגן סלולרי בהשגחה פרטית, איך אפשר לעזור? 
+        לעזרה ברכישת מכשיר לחץ 1
+לעזרה בנושאי שירות לקוחות לחץ 2
+תרצו לדבר עם מנהל מכירות לחץ 3
+        ''']],
         ['([\d{8,15}]|(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})|^(?:00|\\+)[0-9\\s.\\/-]{6,20}$)',['תודה רבה מצוות המגן סלולרי בהשגחה פרטית']]
 
 ]
@@ -65,24 +58,6 @@ def c(x):
 
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming with a simple text message."""
-    resp = MessagingResponse()
-    phoneno = request.form.get('From')
-    msg = request.form.get('Body')
-    if phoneno in obj:
-        chat = Chat(pairs, reflections)
-        resp.message(chat.respond(msg))
-        
-    else:
-        a = Welcome(phoneno)
-        obj.append(a.id)
-
-        resp.message(Welcome.welcome())
-
-    # print(msg)
-
-    return str(resp)
 
 
 
@@ -90,4 +65,4 @@ def sms_reply():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app. run(host='127.0.4.21', port=4040)
