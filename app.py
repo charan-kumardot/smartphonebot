ed6e96188785c1dd6af73e553f6d94ed5e865161
charan-kumardot <59021375+charan-kumardot@users.noreply.github.com>
2021-01-19 09:31:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,31 +9,32 @@ from googletrans import Translator
 app = Flask(__name__, template_folder='templates')
 
 pairs =[
+        ['(1)',['על איזה מכשיר אתם מתעניינים?']],
+        ['(2)',['''
+        לקוח יקר אם ברשותך מערכת סינון והגנה מבית מושגח פלוס
+ נא לפנות בכל פנייה לגבי חנות האפליקציות, להוסיף אפליקציה להגביר רמת סינון וכו'... 
+ אל מוקד שירות לקוחות אפליקצית מושגח 
+בימים א-ה בין השעות 10:00-17:00 : 058-3777779
+ניתן גם לפנות אלינו(מומלץ) גם במייל: mp058377@gmail.com
 
-        ['my name is (.*)| (.*)שמי',['''hi %1 how can i help you?😊 choose the below optoions for the quire
-         1.for buy a smartphone(enter 1) 
-         2.for sell the smartphone(enter 2) 
-         3.for customer service(enter 3)''']],
-        ['(hi|hello|hey|holla|היי|שלום)',['''hey there how can i help you?😊 
-        choose the below optoions for the quires 
-        1.for buy a smartphone(enter 1) 
-        2.for sell the smartphone(enter 2) 
-        3.for customer service(enter 3)''']],
-        ['(1|buy|לקנות את הסמארטפון|buy the smartphone📱|לִקְנוֹת)',['what type of smart phone do you want to buy?']],
-        ['(2|למכור את הסמארטפון|מכירה|sell|sell the smartphone📱)',['what type of smart phone do you want to sell?']],
-        ['(3|שֵׁרוּת|שירות לקוחות|service|customer service|customer)',['enter your number our manager will contact you soon']],
-        ['(ACER|ALCATEL|ALLVIEW|AMAZONAMOI|APPLE|ARCHOS|ASUS|AT&T|BENEFON|BENQ|BENQ-SIEMENS|BIRD|BLACKBERRY|BLACKVIEW|BLUBOSCH|BQ|CASIO|CAT|CELKON|CHEA|COOLPAD|DELL|EMPORIA|ENERGIZER|ERICSSON|ETEN|FAIRPHONE|FUJITSU-SIEMENS|GARMIN-ASUS|GIGABYTE|GIONEE|GOOGLE|HAIER|HONOR|HPHTC|HUAWEI|I-MATE|I-MOBILE|ICEMOBILE|INFINIX|INQ|INTEX|JOLLA|KARBONN|KYOCERA|LAVA|LEECO|LENOVO|LG|MAXON|MAXWEST|MEIZU|MICROMAX|MICROSOFT|MITAC|MITSUBISHI|MODU|MOTOROLA|MWG|NEC|NEONODE|NIU|NOKIA|NVIDIA|ONEPLUS|OPPO|ORANGE|PALM|PANASONIC|PANTECH|PARLA|PHILIPS|PLUMPOSH|PRESTIGIO|QMOBILE|QTEK|RAZER|REALME|SAGEM|SAMSUNG|SENDO|SEWON|SHARP|SIEMENS|SONIM|SONY|SONY ERICSSON|SPICET-MOBILE|TCL|TECNO|TEL.ME.|TELIT|THURAYA|TOSHIBA|ULEFONE|UNNECTOVERT|UVERYKOOL|VIVO|VK MOBILE|VODAFONE|WIKO|WND|XCUTE|XIAOMI|XOLO|YEZZ|YOTA|YUZTE)',['enter your number our manager will contact you soon']],
-        ['(4|אַחֵר|other|other quires)', ['''choose the below optoions for the quires  
-        1.for buy a smartphone(enter 1) 
-        2.for sell the smartphone(enter 2) 
-        3.for customer service(enter 3)''']],
-        ['([\d{8,15}]|(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})|^(?:00|\\+)[0-9\\s.\\/-]{6,20}$)',['Thank you🤗.do you want to ask for other quires (enter 4)']],
-        ['(quite|דַי|bye|QUITE|Bye|ביי)',['Thank you have a nice day.🤗']],
-        ['(.*)', ['''hey, choose the below optoions for the quires
-         1.for buy a smartphone(enter 1) 
-         2.for sell the smartphone(enter 2) 
-         3.for customer service(enter 3)''']]
-        ]
+לקוח נכבד, אם ברשותך מערכת סינון והגנה מבית כושר פליי
+נא לפנות בכל פנייה לגבי חנות האפליקציות, להוסיף אפליקציה להגביר רמת סינון וכו'... 
+אל מוקד שירות לקוחות ווטסאפ (לוחצים על המעטפה) 
+אפליקצית כושר פליי ‏‪053-312-3889‬‏
+ניתן גם לפנות אלינו(מומלץ) גם במייל: kosherplay@gmail.com
+נא לשמור על הקופסא ושטר האחריות
+תקשורת טובה היא שם המשחק תרגישו חופשי לשתף אותנו 
+תשאלו תבררו אנחנו כאן לעזור לכם… במייל ובטלפון איך שנוח לכם.
+        ''']],
+        ['(3)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
+        ['(ACER|ALCATEL|ALLVIEW|AMAZONAMOI|APPLE|ARCHOS|ASUS|AT&T|BENEFON|BENQ|BENQ-SIEMENS|BIRD|BLACKBERRY|BLACKVIEW|BLUBOSCH|BQ|CASIO|CAT|CELKON|CHEA|COOLPAD|DELL|EMPORIA|ENERGIZER|ERICSSON|ETEN|FAIRPHONE|FUJITSU-SIEMENS|GARMIN-ASUS|GIGABYTE|GIONEE|GOOGLE|HAIER|HONOR|HPHTC|HUAWEI|I-MATE|I-MOBILE|ICEMOBILE|INFINIX|INQ|INTEX|JOLLA|KARBONN|KYOCERA|LAVA|LEECO|LENOVO|LG|MAXON|MAXWEST|MEIZU|MICROMAX|MICROSOFT|MITAC|MITSUBISHI|MODU|MOTOROLA|MWG|NEC|NEONODE|NIU|NOKIA|NVIDIA|ONEPLUS|OPPO|ORANGE|PALM|PANASONIC|PANTECH|PARLA|PHILIPS|PLUMPOSH|PRESTIGIO|QMOBILE|QTEK|RAZER|REALME|SAGEM|SAMSUNG|SENDO|SEWON|SHARP|SIEMENS|SONIM|SONY|SONY ERICSSON|SPICET-MOBILE|TCL|TECNO|TEL.ME.|TELIT|THURAYA|TOSHIBA|ULEFONE|UNNECTOVERT|UVERYKOOL|VIVO|VK MOBILE|VODAFONE|WIKO|WND|XCUTE|XIAOMI|XOLO|YEZZ|YOTA|YUZTE|וואן פלוס|סמסונג|שיאומי)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
+        ['([\d{8,15}]|(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})|^(?:00|\\+)[0-9\\s.\\/-]{6,20}$)',['תודה רבה מצוות המגן סלולרי בהשגחה פרטית']],
+        ['(.*)',['''ברוכים הבאים למגן סלולרי בהשגחה פרטית, איך אפשר לעזור? 
+        לעזרה ברכישת מכשיר לחץ 1
+לעזרה בנושאי שירות לקוחות לחץ 2
+תרצו לדבר עם מנהל מכירות לחץ 3''']],
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def samplefunction():
