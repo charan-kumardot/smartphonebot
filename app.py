2157a04a67fd51f932671f71a73a274e840a6c45
charan-kumardot <59021375+charan-kumardot@users.noreply.github.com>
2021-01-19 15:46:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ class welcome:
     def _init_(self,id):
         self.id=id
     def welcome():
-        return '''ברוכים הבאים למגן סלולרי בהשגחה פרטית, איך אפשר לעזור? 
+        return '''
+        ברוכים הבאים למגן סלולרי בהשגחה פרטית, איך אפשר לעזור? 
         לעזרה ברכישת מכשיר לחץ 1
 לעזרה בנושאי שירות לקוחות לחץ 2
-תרצו לדבר עם מנהל מכירות לחץ 3'''
+תרצו לדבר עם מנהל מכירות לחץ 3
+        '''
 obj=[]
 
 pairs =[
@@ -26,7 +28,6 @@ pairs =[
  אל מוקד שירות לקוחות אפליקצית מושגח 
 בימים א-ה בין השעות 10:00-17:00 : 058-3777779
 ניתן גם לפנות אלינו(מומלץ) גם במייל: mp058377@gmail.com
-
 לקוח נכבד, אם ברשותך מערכת סינון והגנה מבית כושר פליי
 נא לפנות בכל פנייה לגבי חנות האפליקציות, להוסיף אפליקציה להגביר רמת סינון וכו'... 
 אל מוקד שירות לקוחות ווטסאפ (לוחצים על המעטפה) 
@@ -39,7 +40,7 @@ pairs =[
         ['(3)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
 
         ['(|טלפון כשר|פלאפון כשר|יוטיוב כשר|סמארטפון כשר|טלפון לילדים|נוקיה 3310 כשר|טאבלט כשר|סלולר כשר|סים כשר|שיאומי כשר|טלפון כשר מאושר|טלפון כשר עם וואטסאפ|טלפון כשר מאושר|מושגח פלוס|פלאפון מושגח|סמארטפון מוגן|וויז כשר|טוקמן|סמארטפון כשר זול|סמארטפון כשר ועדת הרבנים|שיאומי תומך כשר|יוטיוב כשר להורדה|מכשיר כשר|סלולרי כשר|מושגח|טלפונים כשרים|פלאפונים כשרים|ACER|ALCATEL|ALLVIEW|AMAZONAMOI|APPLE|ARCHOS|ASUS|AT&T|BENEFON|BENQ|BENQ-SIEMENS|BIRD|BLACKBERRY|BLACKVIEW|BLUBOSCH|BQ|CASIO|CAT|CELKON|CHEA|COOLPAD|DELL|EMPORIA|ENERGIZER|ERICSSON|ETEN|FAIRPHONE|FUJITSU-SIEMENS|GARMIN-ASUS|GIGABYTE|GIONEE|GOOGLE|HAIER|HONOR|HPHTC|HUAWEI|I-MATE|I-MOBILE|ICEMOBILE|INFINIX|INQ|INTEX|JOLLA|KARBONN|KYOCERA|LAVA|LEECO|LENOVO|LG|MAXON|MAXWEST|MEIZU|MICROMAX|MICROSOFT|MITAC|MITSUBISHI|MODU|MOTOROLA|MWG|NEC|NEONODE|NIU|NOKIA|NVIDIA|ONEPLUS|OPPO|ORANGE|PALM|PANASONIC|PANTECH|PARLA|PHILIPS|PLUMPOSH|PRESTIGIO|QMOBILE|QTEK|RAZER|REALME|SAGEM|SAMSUNG|SENDO|SEWON|SHARP|SIEMENS|SONIM|SONY|SONY ERICSSON|SPICET-MOBILE|TCL|TECNO|TEL.ME.|TELIT|THURAYA|TOSHIBA|ULEFONE|UNNECTOVERT|UVERYKOOL|VIVO|VK MOBILE|VODAFONE|WIKO|WND|XCUTE|XIAOMI|XOLO|YEZZ|YOTA|YUZTE)',['רשום בבקשה את מספר הטלפון שלך ונציג יחזור אליך בהקדם']],
-                ['(ברוך הבא|בוקר טוב|היי|)',['''
+        ['(ברוך הבא|בוקר טוב|היי|)',['''
         ברוכים הבאים למגן סלולרי בהשגחה פרטית, איך אפשר לעזור? 
         לעזרה ברכישת מכשיר לחץ 1
 לעזרה בנושאי שירות לקוחות לחץ 2
@@ -88,12 +89,6 @@ def sms_reply():
     return str(resp)
 
 
-
-
-
-
-if __name__ == '__main__':
-    app. run(host='127.0.4.21', port=4040)
 
 
 
